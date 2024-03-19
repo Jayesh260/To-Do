fa59bb9da16faa29db9ab4d599f869b0a0c8a351
Jayesh Poonia <jp0102@srmist.edu.in>
2024-03-19 06:09:24 +0530

    created flask app template

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
-from flask import Flask,render_template
+from flask import Flask,render_template ,request,redirect,url_for
 
+app=Flask(__name__,template_folder= 'templates')      
+#creating an instance of the class Flask
+
+todo=[]                              #list to store all tasks 
+
+
+@app.route('/')
+#defining a route  for the index page 
+def index():
+    return render_template('index.html')
+
+if __name__== '__main__':
+    app.run(debug=True)
+    
