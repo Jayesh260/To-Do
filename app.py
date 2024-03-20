ebb2bbc9b26c3f3814a33645ec4dd2daed787daa
Jayesh Poonia <jp0102@srmist.edu.in>
2024-03-20 12:57:39 +0530

    did

--- app.py
+++ app.py
@@ -1,17 +1,37 @@
-from flask import Flask,render_template ,request,redirect,url_for
+from flask import Flask, render_template, request, redirect, url_for
 
-app=Flask(__name__,template_folder= 'templates')      
-#creating an instance of the class Flask
+app = Flask(__name__, template_folder='templates')
 
-todo=[]                              #list to store all tasks 
-
+todos = []
 
 @app.route('/')
-#defining a route  for the index page 
 def index():
-    return render_template('index.html')
+    return render_template('index.html', todos=todos)
 
+@app.route('/add', methods=['POST'])
+def add():
+    todo = request.form['todo']
+    todos.append({'task': todo, 'done': False})
+    return redirect(url_for('index'))
 
-if __name__== '__main__':
+@app.route('/edit/<int:index>', methods=['GET', 'POST'])
+def edit(index):
+    todo = todos[index]
+    if request.method == 'POST':
+        todo['task'] = request.form['todo']
+        return redirect(url_for('index'))
+    else:
+        return render_template('edit.html', todo=todo, index=index)
+
+@app.route('/check/<int:index>')
+def check(index):
+    todos[index]['done'] = not todos[index]['done']
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:index>')
+def delete(index):
+    del todos[index]
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
     app.run(debug=True)
-    
